1ab10f8dd1a2953aba66a622bc4e602ed8de4c30
Dmytro Zl <zl.dmitriy94@gmail.com>
2023-11-08 00:14:20 +0200

    Markup with JS only

---
+++ data.js
@@ -0,0 +1,7 @@
+document.writeln("<header>Header <nav>Nav</nav></header>");
+document.writeln("<main>Main");
+document.writeln("<section>");
+document.writeln("        Section");
+document.writeln("</section>");
+document.writeln("</main>");
+document.writeln("<footer>Footer</footer>");
